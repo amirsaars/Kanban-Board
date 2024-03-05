a9dbcd108cb5edab7bf750220066c810180d476f
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2024-03-05 11:59:42 +0330

    vercel analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
 import KanbanBoard from "./components/KanbanBoard";
 
 function App() {
-  return <KanbanBoard />;
+  return (
+    <>
+      <KanbanBoard />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
